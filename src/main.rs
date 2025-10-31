185964a679b44705c0bf3e48498fe9c896d0b836
deathlotuspenta <deathlotuspenta@outlook.com>
2025-10-31 17:04:36 +0800

    day01 init

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // let mut a = 1;
+    // a = 2;
+    // println!("Hello, world!");
+    // println!("a的值:{}",a)
+
+
+
+    // let a = -1;
+    // println!("{}",a)
+
+    // let resutl = 
+    
+    // i:有符号整型 也就是有负数
+    // u:无符号整型，无负数
+
+    // println!("{}",function_study(1))
+
+}
+
+
+// fn add(x: i32, y: i32) -> i32	接收两个 i32，返回一个 i32
+// fn say_hello() -> String	不接收参数，返回一个 String
+// fn print_hi() -> ()	返回空值（等价于 void）
+// 箭头代表返回值
+// 以及传入的参数必须声明类型!!!!!必须写！！！！
+
+fn function_study(x:i32)-> i32{
+
+
+
+    return x+1;
+}
+
